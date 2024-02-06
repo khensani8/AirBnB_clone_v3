a4c493dad4d219975b0894e179ff2194360cb00d
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2024-02-06 02:37:23 +0200

    users api

---
+++ api/v1/views/users.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+'''
+Create a new view for User objects - handles all default RESTful API actions
+'''
+from flask import jsonify, request, abort
+from models import storage
+from api.v1.views import app_views
+from models.user import User
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_users():
+    users = storage.all(User).values()
+    return jsonify([user.to_dict() for user in users])
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
+
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def delete_user(user_id):
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    user.delete()
+    storage.save()
+    return jsonify({})
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    data = request.get_json()
+    if 'email' not in data:
+        abort(400, description="Missing email")
+    if 'password' not in data:
+        abort(400, description="Missing password")
+    new_user = User(**data)
+    new_user.save()
+    return jsonify(new_user.to_dict()), 201
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user(user_id):
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+    data = request.get_json()
+    ignore_keys = ['id', 'email', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(user, key, value)
+    user.save()
+    return jsonify(user.to_dict())
